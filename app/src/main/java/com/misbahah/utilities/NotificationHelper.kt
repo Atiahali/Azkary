c8f672bd6dda60b0b30ea4a284edabe8cd029c57
Atiahali <aaadddccc2005@gmail.com>
2021-06-12 13:25:14 +0300

    make makeStatusNotification method to receive pending intent

--- app/src/main/java/com/misbahah/utilities/NotificationHelper.kt
+++ app/src/main/java/com/misbahah/utilities/NotificationHelper.kt
@@ -2,23 +2,23 @@ package com.misbahah.utilities
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.misbahah.R
-import com.misbahah.broadcast.MyReceiver.Companion.CHANNEL_ID
-import com.misbahah.broadcast.MyReceiver.Companion.NOTIFICATION_ID
-import com.misbahah.broadcast.MyReceiver.Companion.NOTIFICATION_TITLE
-import java.lang.NullPointerException
+import com.misbahah.ui.main.NotificationWorker.Companion.CHANNEL_ID
+import com.misbahah.ui.main.NotificationWorker.Companion.NOTIFICATION_ID
+import com.misbahah.ui.main.NotificationWorker.Companion.NOTIFICATION_TITLE
 
-fun makeStatusNotification(message: String, context: Context, channel: NotificationChannel?) {
+fun makeStatusNotification(message: String, context: Context, channel: NotificationChannel?, pendingIntent: PendingIntent?) {
 
     // Make a channel if necessary
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         // Add the channel
         val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (channel != null)
             notificationManager.createNotificationChannel(channel)
         else
@@ -27,12 +27,16 @@ fun makeStatusNotification(message: String, context: Context, channel: Notificat
 
     // Create the notification
     val builder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_tasbih)
-            .setContentTitle(NOTIFICATION_TITLE)
-            .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setVibrate(LongArray(1))
-            .setCategory(NotificationCompat.CATEGORY_ALARM)
+        .setSmallIcon(R.drawable.ic_tasbih)
+        .setContentTitle(NOTIFICATION_TITLE)
+        .setContentText(message)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setVibrate(LongArray(1))
+        .setCategory(NotificationCompat.CATEGORY_ALARM)
+
+    if(pendingIntent != null) {
+        builder.setContentIntent(pendingIntent)
+    }
 
     // Show the notification
     NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
