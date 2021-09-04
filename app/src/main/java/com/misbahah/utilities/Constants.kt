74de32049564212f9244e3c8a20bc27fd8ad08ee
Attia Ali <aaadddccc2005@gmail.com>
2021-09-04 11:30:20 +0300

    Removes TOP_TIMES_OF_ZIKR_KEY and WORKER_PREFERECES constants from Constants.kt

--- app/src/main/java/com/misbahah/utilities/Constants.kt
+++ app/src/main/java/com/misbahah/utilities/Constants.kt
@@ -1,6 +1,4 @@
 package com.misbahah.utilities
 
 const val APPLICATION_PREFS = "application_prefs"
-const val TOP_TIMES_OF_ZIKR_KEY = "zikr_key"
 const val FIRST_RUN_KEY = "first-run"
-const val WORKER_PREFERECES = "worker-preferences"
